e72c77eb1c627f65e380aaefa239baf5b8f2c91f
sndmaa <sanclaw55@gmail.com>
2022-07-18 15:21:36 +0100

    first_commit

--- user/routes.py
+++ user/routes.py
@@ -1,10 +1,14 @@
 from app import app
 from user.models import User
+from token_required import token_required
+
 
 @app.route('/register', methods=['POST',])
+@token_required
 def signup():
     return User().signup()
 
 @app.route('/login', methods=['POST',])
+@token_required
 def login():
     return User().login()
